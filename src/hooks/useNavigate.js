23bf2c75d6d67f3963a6fc3731e2abaa05dc30b4
Erica Garand <egarand13@outlook.com>
2024-10-07 18:43:59 -0400

    fix bug in useNavigate hook; imported the wrong function name

--- src/hooks/useNavigate.js
+++ src/hooks/useNavigate.js
@@ -3,14 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { AnnouncementContext } from "../App";
 
 /** An extension of react router's `useNavigate` which will load the chosen route, while also announcing the reload to screen reader users.
- * @see `useNavigate` from react-router-dom
- */
+ * @see `useNavigate` from react-router-dom */
 export function useNavigate() {
-	const {setAnnouncement, setUserNavigated} = useContext(AnnouncementContext);
+	const {setAnnouncement, userNavigated} = useContext(AnnouncementContext);
 	const navigate = useNavigate();
 
 	return (to, options = {}) => {
-		setUserNavigated(true);
+		userNavigated();
 		setAnnouncement("Loading new page...");
 		navigate(to, options);
 	}
