6158370c63cef5635abfc8d6c9c21c36c5721dac
Erica Garand <egarand13@outlook.com>
2024-10-08 11:48:16 -0400

    fix bug in custom useNavigate hook; needed to alias an import

--- src/hooks/useNavigate.js
+++ src/hooks/useNavigate.js
@@ -1,12 +1,12 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate as rrUseNavigate } from "react-router-dom";
 import { AnnouncementContext } from "../App";
 
 /** An extension of react router's `useNavigate` which will load the chosen route, while also announcing the reload to screen reader users.
  * @see `useNavigate` from react-router-dom */
 export function useNavigate() {
 	const {setAnnouncement, userNavigated} = useContext(AnnouncementContext);
-	const navigate = useNavigate();
+	const navigate = rrUseNavigate();
 
 	return (to, options = {}) => {
 		userNavigated();
