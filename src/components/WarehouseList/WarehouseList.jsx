abc39071b5e381abec09c6c1de92e90e24ca40dd
Viswa Harini Vidyashankar <viswavidya16@gmail.com>
2024-10-10 02:20:58 -0400

    build warehouse-list

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,9 +1,84 @@
 import "./WarehouseList.scss";
+import deleteIcon from '../../assets/icons/delete_outline-24px.svg';
+import editIcon from '../../assets/icons/edit-24px.svg';
+import sortIcon from '../../assets/icons/sort-24px.svg';
+import viewDetailsIcon from '../../assets/icons/chevron_right-24px.svg';
+import Button from '../../components/Button/Button'
+import Input from '../../components/Input/Input'
+import MainCard from '../../components/MainCard/MainCard'
+import Link from "../Link/Link";
 
-function WarehouseList() {
+function WarehouseList({data}) {
+	
 	return (
-		<div>WarehouseComponent</div>
-		
+		<section className='warehouse-component'>
+			<section className='warehouse-list'>
+				<div className='warehouse-list__header'>
+					<h2 className='warehouse-list__title'>Warehouses</h2>
+					<div className='warehouse-list__inputs'>
+						<Input inputClassName='warehouse-list__search-bar' placeholder='Search...' type='search' label='' onChange=''></Input>
+						<Button className='warehouse-list__add-warehouse' to ='/warehouses/add' variant='primary' >Add New Warehouse</Button>
+					</div>
+				</div>
+				<div className='warehouse-list__records-container'>
+					<div className='warehouse-list__label'>
+						<ul className='warehouse-list__label-list'>
+							<li className='warehouse-list__label-item'>WAREHOUSE
+								<img src={sortIcon} className='warehouse-list__label-sort' />
+							</li>
+							<li className='warehouse-list__label-item'>ADDRESS
+								<img src={sortIcon} className='warehouse-list__label-sort' />
+							</li>
+							<li className='warehouse-list__label-item'>CONTACT NAME
+								<img src={sortIcon} className='warehouse-list__label-sort' />
+							</li>
+							<li className='warehouse-list__label-item'>CONTACT INFORMATION
+								<img src={sortIcon} className='warehouse-list__label-sort' />
+							</li>
+							<li className='warehouse-list__label-item'>ACTIONS
+							</li>
+						</ul>
+					</div>
+					{
+					data.map((item) => {
+						return (
+							<article className='warehouse-record' key={item.id}>
+								<ul className='warehouse-record__list'>
+									<ul className='warehouse-record__sub-list'>
+										<li className='warehouse-record__list-item'>
+											<h4 className='warehouse-record__label'>WAREHOUSE</h4>
+											<div className='warehouse-record__info'>
+												<Link to={`/warehouses/${item.id}`} className='warehouse-record__info-details--link'><p>{item.warehouse_name}</p></Link>
+												<img className='warehouse-list__view-details' src={viewDetailsIcon} alt='view warehouse detail button' />
+											</div>
+										</li>
+										<li className='warehouse-record__list-item'>
+											<h4 className='warehouse-record__label'>ADDRESS</h4>
+											<p className='warehouse-record__info-details'>{`${item.address}, ${item.city}, ${item.country}`}</p>
+										</li>
+									</ul>
+									<ul className='warehouse-record__sub-list'>
+										<li className='warehouse-record__list-item'>
+											<h4 className='warehouse-record__label'>CONTACT NAME</h4>
+											<p className='warehouse-record__info-details'>{item.contact_name}</p>
+										</li>
+										<li className='warehouse-record__list-item'>
+											<h4 className='warehouse-record__label'>CONTACT INFORMATION</h4>
+											<p className='warehouse-record__info-details'>{`${item.contact_phone} ${item.contact_email}`}</p>
+										</li>
+									</ul>
+								</ul>
+								<div className='warehouse-record__actions'>
+									<Link to=''><img className='warehouse-record__delete' src={deleteIcon} alt='delete icon' /></Link>
+									<Link to={`/warehouses/edit/${item.id}`}><img className='warehouse-record__edit' src={editIcon} alt='edit icon' /></Link>
+								</div>	
+							</article>
+						)
+					})
+				}
+				</div>
+			</section>
+		</section>
 	);
 }
 
