c38f9450139b99b5e62096ebcf6e5591095ecb89
Erica Garand <egarand13@outlook.com>
2024-10-10 13:54:52 -0400

    Add delete modal to WarehouseListPage

--- src/pages/WarehouseListPage/WarehouseListPage.jsx
+++ src/pages/WarehouseListPage/WarehouseListPage.jsx
@@ -1,7 +1,8 @@
-import WarehouseList from "../../components/WarehouseList/WarehouseList";
 import "./WarehouseListPage.scss";
-import { useEffect, useState } from 'react'
+import WarehouseList from "../../components/WarehouseList/WarehouseList";
+import Modal from "../../components/Modal/Modal";
 import axios from 'axios';
+import { useCallback, useEffect, useState } from 'react';
 
 function WarehouseListPage() {
 	const [warehouseList, setWarehouseList] = useState([]);
@@ -9,19 +10,42 @@ function WarehouseListPage() {
     const [isLoading, setIsLoading] = useState(true);
 	const url = import.meta.env.VITE_API_URL;
 
+	const [modalOpen, setModalOpen] = useState(false),
+        [modalItem, setModalItem] = useState(null);
+
+    const getWarehouseList = useCallback(async () => {
+        setIsLoading(true);
+        setIsError(false);
+        try {
+            const {data} = await axios.get(`${url}/api/warehouses/`);
+            setWarehouseList(data);
+            setIsLoading(false);
+        } catch (error){
+            console.log(`Could not fetch warehouse list ${error}`);
+            setIsError(true);
+        }
+    }, [url]);
+
 	useEffect(() => {
-        const getWarehouseList = async () => {
-            try {
-                const {data} = await axios.get(`${url}/api/warehouses/`);
-                setWarehouseList(data);
-                setIsLoading(false);
-            } catch (error){
-                setIsError(true);
-                console.log(`Could not fetch warehouse list ${error}`)
-            }
-            }
-            getWarehouseList(); 
-    }, [])
+        getWarehouseList();
+    }, [getWarehouseList]);
+
+	function showDeleteModal(warehouse) {
+		setModalItem(warehouse);
+		setModalOpen(true);
+	}
+
+	async function deleteWarehouse(id) {
+		if (!id) { return; }
+		try {
+			await axios.delete(
+				`${import.meta.env.VITE_API_URL}/api/warehouses/${id}`
+			);
+			getWarehouseList();
+		} catch (error) {
+            console.error(error);
+		}
+	}
 
     if(isError) {
         return <h1>Sorry, there was some error in fetching the list</h1>
@@ -31,9 +55,18 @@ function WarehouseListPage() {
         return <h1>Loading list...</h1>
     }
 
-	return (
-		<WarehouseList data={warehouseList}></WarehouseList>
-	);
+	return (<>
+		<WarehouseList data={warehouseList} deleteItemFn={showDeleteModal}/>
+        <Modal
+            isOpen={modalOpen}
+            setIsOpen={setModalOpen}
+            title={`Delete ${modalItem?.warehouse_name} warehouse?`}
+            content={`Please confirm that you'd like to delete the ${
+                modalItem?.warehouse_name
+            } from the list of warehouses. You won't be able to undo this action.`}
+            onDelete={async () => await deleteWarehouse(modalItem?.id)} />
+        <ToastContainer/>
+	</>);
 }
 
 export default WarehouseListPage;
