62d7d06c178100dfba217b49148f548a8a9cc57c
Erica Garand <egarand13@outlook.com>
2024-10-12 13:37:54 -0400

    add deletion modal to inventory list on  WarehousePage

--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -2,13 +2,13 @@ import "./WarehousePage.scss";
 import MainCard from "../../components/MainCard/MainCard";
 import PageHeader from "../../components/PageHeader/PageHeader";
 import InventoryList from "../../components/InventoryList/InventoryList";
+import Modal from "../../components/Modal/Modal";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useParams, Link } from "react-router-dom";
 import editicon from "../../assets/icons/edit-white-24px.svg";
-import { useNavigate } from "../../hooks/useNavigate";
 
 const initialValues = {
 	warehouse_name: "",
@@ -24,22 +24,17 @@ const initialValues = {
 function WarehousePage() {
   const block = "warehouse-details-page";
   const { warehouseID } = useParams(); //Get the warehouse id
-  const navigate = useNavigate();
   const [values, setValues] = useState(initialValues);
   const [inventoryList,setInventoryList] = useState([]);
   const [warehouseMap,setWarehouseMap] = useState({});
   const [isLoading,setIsLoading] = useState(true);
   const [isError,setIsError] = useState(false);
 
-  useEffect(() => {
-    if (warehouseID) {
-      fetchWarehouseDetails(warehouseID);
-      fetchWarehouseInventory(warehouseID);
-    }
-  }, [warehouseID]);
+	const [modalOpen, setModalOpen] = useState(false),
+        [modalItem, setModalItem] = useState(null);
 
   // Function to fetch warehouse
-  async function fetchWarehouseDetails(id) {
+  const fetchWarehouseDetails = useCallback(async (id) => {
     try {
       const response = await axios.get(
         `${import.meta.env.VITE_API_URL}/api/warehouses/${id}`
@@ -49,23 +44,30 @@ function WarehousePage() {
     } catch (error) {
       handleFetchError(error); // Handle any errors
     }
-  }
+  }, []);
 
   // Function to fetch inventory for the warehouse
-  async function fetchWarehouseInventory(id) {
+  const fetchWarehouseInventory = useCallback(async (id) => {
     try {
         const response = await axios.get(
             `${import.meta.env.VITE_API_URL}/api/warehouses/${id}/inventories`
         );
         console.log("Inventories for the warehouse:",response.data);
-        setInventoryList(response.data); 
-        setIsLoading(false); 
+        setInventoryList(response.data);
+        setIsLoading(false);
     } catch (error) {
         console.error(error);
-        setIsError(true); 
+        setIsError(true);
         handleFetchError(error);
     }
-}
+  }, []);
+
+  useEffect(() => {
+    if (warehouseID) {
+      fetchWarehouseDetails(warehouseID);
+      fetchWarehouseInventory(warehouseID);
+    }
+  }, [warehouseID, fetchWarehouseDetails, fetchWarehouseInventory]);
 
   // Improved error handling
   function handleFetchError(error) {
@@ -89,6 +91,37 @@ function WarehousePage() {
     }
   }
 
+	function showDeleteModal(item) {
+		setModalItem(item);
+		setModalOpen(true);
+	}
+
+  async function deleteInventoryItem(id) {
+    if (!id) { return; }
+    try {
+      await axios.delete(`${import.meta.env.VITE_API_URL}/api/inventories/${id}`);
+      toast.success("Item deleted.");
+      fetchWarehouseInventory(warehouseID);
+      } catch (error) {
+        console.error(error);
+        let errorMessage;
+        if (error.response) {
+          if (error.response.status === 404) {
+            errorMessage = "Item not found.";
+          } else {
+            errorMessage =
+            `Error: ${error.response.data.message || "Unknown error"}`;
+          }
+        } else if (error.request) {
+          errorMessage =
+            "No response from the server. Please check your network connection.";
+        } else {
+          errorMessage = `Error: ${error.message}`;
+        }
+        toast.error(errorMessage);
+      }
+  }
+
   return (
     <MainCard className={block}>
 		<div className={`${block}__head`}>
@@ -104,7 +137,7 @@ function WarehousePage() {
 				<div className={`${block}__container`}>
 					<label className={`${block}__label`}>WAREHOUSE ADDRESS:</label>
 					<div  className={`${block}__address-container`}>
-						<p className={`${block}__values`}>{values.address},</p> 
+						<p className={`${block}__values`}>{values.address},</p>
 						<p className={`${block}__values`}>{values.city}, {values.country}</p>
 					</div>
 				</div>
@@ -127,8 +160,16 @@ function WarehousePage() {
             ) : isError ? (
                 <p>Sorry, there was an error fetching the inventory list.</p>
             ) : (
-                <InventoryList data={inventoryList} warehouseMap={warehouseMap} showFlag={false} />
+                <InventoryList data={inventoryList} warehouseMap={warehouseMap} showFlag={false} deleteItemFn={showDeleteModal} />
             )}
+      <Modal
+        isOpen={modalOpen}
+        setIsOpen={setModalOpen}
+        title={`Delete ${modalItem?.item_name} inventory item?`}
+        content={`Please confirm that you'd like to delete ${
+            modalItem?.item_name
+        } from the inventory list. You won't be able to undo this action.`}
+        onDelete={async () => await deleteInventoryItem(modalItem?.id)} />
       <ToastContainer />
 	</MainCard>
   );
