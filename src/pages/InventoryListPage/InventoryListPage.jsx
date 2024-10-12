ca377587594392d5b413babaa33f4c0d0fa796c4
Erica Garand <egarand13@outlook.com>
2024-10-12 01:32:05 -0400

    add delete modal to InventoryListPage

--- src/pages/InventoryListPage/InventoryListPage.jsx
+++ src/pages/InventoryListPage/InventoryListPage.jsx
@@ -2,20 +2,27 @@ import InventoryList from "../../components/InventoryList/InventoryList.jsx";
 import "./InventoryListPage.scss";
 import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
+import { toast, ToastContainer } from "react-toastify";
+import Modal from "../../components/Modal/Modal.jsx";
+import "react-toastify/dist/ReactToastify.css";
 
 function InventoryListPage() {
 	const [inventoriesList, setInventoriesList] = useState([]);
     const [warehouseMap, setWarehouseMap] = useState({});
 	const [isError, setIsError] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
+    const url = import.meta.env.VITE_API_URL;
+
+	const [modalOpen, setModalOpen] = useState(false),
+        [modalItem, setModalItem] = useState(null);
 
     const fetchData = useCallback(async () => {
-        const url = import.meta.env.VITE_API_URL;
+        setIsLoading(true);
+        setIsError(false);
         const getInventoriesList = async () => {
             try {
                 const {data} = await axios.get(`${url}/api/inventories/`);
                 setInventoriesList(data);
-                setIsLoading(false);
             } catch (error){
                 console.error(error);
                 setIsError(true);
@@ -37,23 +44,62 @@ function InventoryListPage() {
             }
         };
         await Promise.all([getInventoriesList(), getWarehouseList()]);
-    }, []);
+        setIsLoading(false);
+    }, [url]);
 
 	useEffect(() => {
         fetchData();
     }, [fetchData]);
 
-    if(isError) {
-        return <h1>Sorry, there was some error in fetching the list</h1>
+	function showDeleteModal(item) {
+		setModalItem(item);
+		setModalOpen(true);
+	}
+
+    async function deleteInventoryItem(id) {
+        if (!id) { return; }
+        try {
+            await axios.delete(`${url}/api/inventories/${id}`);
+            toast.success("Item deleted.");
+            fetchData();
+        } catch (error) {
+            console.error(error);
+			let errorMessage;
+			if (error.response) {
+				if (error.response.status === 404) {
+					errorMessage = "Item not found.";
+				} else {
+					errorMessage =
+					`Error: ${error.response.data.message || "Unknown error"}`;
+				}
+			} else if (error.request) {
+				errorMessage =
+					"No response from the server. Please check your network connection.";
+			} else {
+				errorMessage = `Error: ${error.message}`;
+			}
+			toast.error(errorMessage);
+        }
     }
 
-    if(isLoading) {
-        return <h1>Loading list...</h1>
-    }
+	return (<>
+        {isError && <h1>Sorry, there was some error in fetching the list</h1>}
 
-	return (
-		<InventoryList data={inventoriesList} warehouseMap={warehouseMap} showFlag={true}></InventoryList>
-	);
+        {isLoading && !isError && <h1>Loading list...</h1>}
+
+        {!isLoading && !isError
+        && <InventoryList data={inventoriesList} warehouseMap={warehouseMap} showFlag={true} deleteItemFn={showDeleteModal}/>}
+
+        <Modal
+            isOpen={modalOpen}
+            setIsOpen={setModalOpen}
+            title={`Delete ${modalItem?.item_name} inventory item?`}
+            content={`Please confirm that you'd like to delete ${
+                modalItem?.item_name
+            } from the inventory list. You won't be able to undo this action.`}
+            onDelete={async () => await deleteInventoryItem(modalItem?.id)} />
+        <ToastContainer/>
+	</>);
 }
 
 export default InventoryListPage;
